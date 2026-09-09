50e1f56c02106dea46e844d50ae2d908d8d407c0
thapedibrandon02-cmd <thapedibrandon02@gmail.com>
2026-09-09 15:21:01 +0200

    Create Program.cs

---
+++ cyberSecurityAwarenessBot/cyberSecurityAwarenessBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using cyberSecurityAwarenessBot;
+using System;
+
+namespace CybersecurityAwarenessBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chatbot bot = new Chatbot();
+            bot.Start();
+        }
+    }
+}
